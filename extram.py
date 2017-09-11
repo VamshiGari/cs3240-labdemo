7b0deb6a9628b833a5bf26b81da2f18436f364f8
Vamshi <vkg5xt@virginia.edu>
2017-09-11 14:32:20 -0400

    added new file extram.py

---
+++ extram.py
@@ -0,0 +1,6 @@
+from helper import *
+
+if __name__ == '__main__':
+    greeting("hello")
+    greeting_master("this is part of the master")
+    greeting_master("branch")
--- helper.py
+++ helper.py
@@ -1,2 +1,10 @@
 def greeting(msg):
     print(msg)
+
+
+def greeting_newbranch(msg):
+    print(msg + "!")
+
+
+def greeting_master(msg):
+    print(msg + ".")
